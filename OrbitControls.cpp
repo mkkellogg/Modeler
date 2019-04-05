ae479acce55b39d06a7975d6f8e44bac395eeef3
mkkellogg <mkkellogg@gmail.com>
2019-04-05 00:14:09 -0700

    More solid OrbitControls

--- OrbitControls.cpp
+++ OrbitControls.cpp
@@ -43,90 +43,75 @@ void OrbitControls::handleGesture(GestureAdapter::GestureEvent event) {
         Core::WeakPointer<Core::Graphics> graphics = this->engine->getGraphicsSystem();
         Core::WeakPointer<Core::Renderer> rendererPtr = graphics->getRenderer();
 
-        Core::Vector4u viewport = graphics->getViewport();
-        Core::Real ndcStartX = (Core::Real)eventStartX / (Core::Real)viewport.z * 2.0f - 1.0f;
-        Core::Real ndcStartY = (Core::Real)eventStartY / (Core::Real)viewport.w * 2.0f - 1.0f;
-        Core::Real ndcEndX = (Core::Real)eventEndX / (Core::Real)viewport.z * 2.0f - 1.0f;
-        Core::Real ndcEndY = (Core::Real)eventEndY / (Core::Real)viewport.w * 2.0f - 1.0f;
-        Core::Real ndcZ = eventPointer == GestureAdapter::GesturePointer::Tertiary ? 0.5f : 0.0f;
-
         Core::WeakPointer<Core::Object3D> cameraObject = this->targetCamera->getOwner();
         cameraObject->getTransform().updateWorldMatrix();
         Core::Matrix4x4 camWorldMat = cameraObject->getTransform().getWorldMatrix();
-
-        Core::Point3r worldStartP(ndcStartX, ndcEndY, ndcZ);
-        Core::Point3r worldEndP(ndcEndX, ndcStartY, ndcZ);
-        this->targetCamera->unProject(worldStartP);
-        this->targetCamera->unProject(worldEndP);
-        camWorldMat.transform(worldStartP);
-        camWorldMat.transform(worldEndP);
 
         Core::Point3r cameraPos;
         camWorldMat.transform(cameraPos);
         Core::Vector3r cameraToOrigin = this->origin - cameraPos;
         Core::Point3r originRelativeCameraPos(-cameraToOrigin.x, -cameraToOrigin.y, -cameraToOrigin.z);
 
-        Core::Vector3r cameraPosVec(originRelativeCameraPos.x, originRelativeCameraPos.y, originRelativeCameraPos.z);
-        cameraPosVec.normalize();
-        Core::Vector3r up = Core::Vector3r::Up;
-        Core::Real curDot = up.dot(cameraPosVec);
-
-        Core::Vector3r cameraToAdjustedOrigin = cameraToOrigin;
-        cameraToAdjustedOrigin.normalize();
-        cameraToAdjustedOrigin = cameraToAdjustedOrigin * 5.0f;
-        Core::Point3r adjustedOrigin = cameraPos + cameraToAdjustedOrigin;
-
-        Core::Vector3r adjustedWorldStartV = worldStartP - adjustedOrigin;
-        Core::Vector3r adjustedWorldEndV = worldEndP - adjustedOrigin;
-
-        Core::Vector3r adjustedWorldStartVN = Core::Vector3r(adjustedWorldStartV.x, adjustedWorldStartV.y, adjustedWorldStartV.z);
-        adjustedWorldStartVN.normalize();
-        Core::Vector3r adjustedWorldEndVN = Core::Vector3r(adjustedWorldEndV.x, adjustedWorldEndV.y, adjustedWorldEndV.z);
-        adjustedWorldEndVN.normalize();
-
         if (eventPointer == GestureAdapter::GesturePointer::Secondary) {
 
-            Core::Real oldDot =  adjustedWorldStartVN.dot(up);
-            Core::Real newDot =  adjustedWorldEndVN.dot(up);
+            Core::Real theta = ((Core::Real)eventEndX - (Core::Real)eventStartX) * -.0001f;
+            Core::Real phi = ((Core::Real)eventEndY - (Core::Real)eventStartY) * -.0001f;
+            Core::Real cosPhi = Core::Math::cos(phi);
 
-            if (!(curDot <= .999 || newDot  > oldDot)) {
-                adjustedWorldStartVN.y = 0.0f;
-                adjustedWorldStartVN.normalize();
-                adjustedWorldEndVN.y = 0.0f;
-                adjustedWorldEndVN.normalize();
-            }
+            Core::Vector3r cameraPosVec(originRelativeCameraPos.x, originRelativeCameraPos.y, originRelativeCameraPos.z);
+            cameraPosVec.normalize();
+            Core::Real cameraPosUpDot = cameraPosVec.dot(Core::Vector3r::Up);
 
-            Core::Real cosRotationAngle = Core::Vector3r::dot(adjustedWorldStartVN, adjustedWorldEndVN);
-            Core::Real rotationAngle = 0.0f;
-            if (cosRotationAngle < 1.0f && cosRotationAngle > -1.0f) {
-                rotationAngle = Core::Math::aCos(cosRotationAngle);
-            }
-            Core::Vector3r rotAxis;
-            Core::Vector3r::cross(adjustedWorldEndVN, adjustedWorldStartVN, rotAxis);
-            rotAxis.normalize();
+            if (theta != 0.0f || phi != 0.0f) {
 
-            if (rotationAngle > 0.0f) {
+                Core::Real epsilon = 0.01f;
+                Core::Real rotationScaleFactor = 25.0f;
 
-                Core::Real rotationScaleFactor = 30.0f * (1.0f - curDot/1.5f);
-                rotationScaleFactor = 20.0f;
-
-                Core::Quaternion qA = Core::Quaternion::fromAngleAxis(rotationAngle * rotationScaleFactor, rotAxis);
-                Core::Matrix4x4 rot = qA.rotationMatrix();
+                Core::Quaternion quatTheta = Core::Quaternion::fromAngleAxis(theta * rotationScaleFactor, Core::Vector3r::Up);
+                Core::Vector3r phiAxis = Core::Vector3r::Right;
+                camWorldMat.transform(phiAxis);
+                Core::Real finalPhi = phi * rotationScaleFactor;
+                Core::Quaternion quatPhi = Core::Quaternion::fromAngleAxis(finalPhi, phiAxis);
+                Core::Matrix4x4 rotTheta = quatTheta.rotationMatrix();
+                Core::Matrix4x4 rotPhi = quatPhi.rotationMatrix();
 
                 Core::Matrix4x4 worldTransformation;
                 worldTransformation.preTranslate(-this->origin.x, -this->origin.y, -this->origin.z);
-                worldTransformation.preMultiply(rot);
+                Core::Real totalPhi = Core::Math::aCos(cameraPosUpDot) + finalPhi;
+                if ((totalPhi < Core::Math::PI - epsilon || phi < 0.0f) && (totalPhi > epsilon || phi > 0.0f)) worldTransformation.preMultiply(rotPhi);
+                worldTransformation.preMultiply(rotTheta);
                 worldTransformation.preTranslate(this->origin.x, this->origin.y, this->origin.z);
 
                 cameraObject->getTransform().transformBy(worldTransformation, Core::TransformationSpace::World);
-                cameraObject->getTransform().lookAt(this->origin, up);
+                cameraObject->getTransform().lookAt(this->origin, Core::Vector3r::Up);
                 this->lastMoveX = event.end.x;
                 this->lastMoveY = event.end.y;
-
             }
-
         }
         else if (eventPointer == GestureAdapter::GesturePointer::Tertiary) {
+
+            Core::Vector4u viewport = graphics->getViewport();
+            Core::Real ndcStartX = (Core::Real)eventStartX / (Core::Real)viewport.z * 2.0f - 1.0f;
+            Core::Real ndcStartY = (Core::Real)eventStartY / (Core::Real)viewport.w * 2.0f - 1.0f;
+            Core::Real ndcEndX = (Core::Real)eventEndX / (Core::Real)viewport.z * 2.0f - 1.0f;
+            Core::Real ndcEndY = (Core::Real)eventEndY / (Core::Real)viewport.w * 2.0f - 1.0f;
+            Core::Real ndcZ = eventPointer == GestureAdapter::GesturePointer::Tertiary ? 0.5f : 0.0f;
+
+            Core::Point3r worldStartP(ndcStartX, ndcEndY, ndcZ);
+            Core::Point3r worldEndP(ndcEndX, ndcStartY, ndcZ);
+            this->targetCamera->unProject(worldStartP);
+            this->targetCamera->unProject(worldEndP);
+            camWorldMat.transform(worldStartP);
+            camWorldMat.transform(worldEndP);
+
+            Core::Vector3r cameraToAdjustedOrigin = cameraToOrigin;
+            cameraToAdjustedOrigin.normalize();
+            cameraToAdjustedOrigin = cameraToAdjustedOrigin * 5.0f;
+            Core::Point3r adjustedOrigin = cameraPos + cameraToAdjustedOrigin;
+
+            Core::Vector3r adjustedWorldStartV = worldStartP - adjustedOrigin;
+            Core::Vector3r adjustedWorldEndV = worldEndP - adjustedOrigin;
+
             Core::Real translationScaleFactor = originRelativeCameraPos.magnitude();
             Core::Vector3r viewDragVector = adjustedWorldEndV - adjustedWorldStartV;
             viewDragVector.invert();
